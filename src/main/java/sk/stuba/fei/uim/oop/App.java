970a1e01c6d0c11315df2665f05f5f65b172bcd4
Tomas-Kamensky <xkamenskyt@stuba.sk>
2022-02-20 20:45:20 +0100

    Pridanie elementarnych premennych

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -4,5 +4,16 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello world");
 
+        int celeCislo = 5;
+        long celeCisloVacsie = 5L;
+        float desatinneCislo = 5.5f;
+        double desatinneCisloDlhsie = 5.5;
+        char znak = 'c';
+        String retazecZnakov = "text v uvodzovkach";
+        boolean bool = true;
+
+        int podiel = 2/3; // = 0
+ float zvysok = 2%3.0f;
+
     }
 }
